d7c8fe3536b6d25f9b301125b9df2580179b3bee
Stewart Duffy <duffy.stewart@gmail.com>
2020-06-28 11:40:45 +1200

    Updates order of ID's for correct grouping

--- src/components/TreeLevel/TreeLevel.tsx
+++ src/components/TreeLevel/TreeLevel.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import Person from "../Person/Person";
+import styles from "./TreeLevel.module.css"; // Import css modules stylesheet as styles
 
 const TreeLevel = ({ people }: any) => {
   return (
     <div>
       {people.map((person: any) => {
-        console.log("person: ", person);
-
         return (
-          <ul key={`person=${person.id}`}>
+          <ul key={`person=${person.id}`} className={styles["tree-level"]}>
             <Person {...person} />
 
             {person.children && person.children.length > 0 && (
-              <TreeLevel people={person.children} />
+              <div className={styles["children"]}>
+                <TreeLevel people={person.children} />
+              </div>
             )}
           </ul>
         );
--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,24 @@
 export const buildTreeData = (data: any) => {
   const preProcess = (data: any) => {
-    return data.map((person: any) => {
-      return {
-        ...person,
-        childrenIds: person.children,
-        parentId: person.parents[0] || null,
-      };
-    });
+    return (
+      data
+        //Order ID's
+        .map(({ children, parents, ...restOfperson }: any) => {
+          return {
+            ...restOfperson,
+            children: [...children].sort(),
+            parents: [...parents].sort(),
+          };
+        })
+        //Store children ID's & single parents ID
+        .map((person: any) => {
+          return {
+            ...person,
+            childrenIds: person.children,
+            parentId: person.parents[0] || null,
+          };
+        })
+    );
   };
 
   const getChildren = (people: any, id = null, link = "parentId") => {
